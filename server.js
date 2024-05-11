dcbee6bdabe9f59377cd94a3de5d92db660afce8
NawodaRanasingha <nawodaranasinha@gmail.com>
2024-05-11 17:59:06 +0530

    db connection

---
+++ server.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const express =require('express');
+const cookieParser = require('cookie-parser');
+
+const cors = require('cors');
+const routes = require('./routes/routes')
+
+const app = express();
+app.use(cors({
+    credentials: true,
+    origin: ['http://localhost:4200']
+}));
+app.use(cookieParser())
+
+app.use(express.json)
+app.use("/api", routes)
+
+mongoose.connect("mongodb://localhost:27017/saloon")
+    .then(() => {
+        console.log("connected to database")
+        app.listen(5000, () => {
+            console.log("App is listening on port 5000")
+        })
+    })
