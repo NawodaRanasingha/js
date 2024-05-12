e30226b1b3ab1220f8c4d86cb24d02e6407090f9
NawodaRanasingha <nawodaranasinha@gmail.com>
2024-05-12 10:15:54 +0530

    set connection

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,10 @@
 const { Router } = require('express');
 
+var studentController = require('.././src/User/userControll');
 const router = Router();
 
 
+router.route('/student/login').post(studentController.loginUserControllerFn);
+router.route('/User/create').post(userControll.createuserControllFn);
 
 module.exports = router;
